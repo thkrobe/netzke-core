67d4b2cce1bf8b2eacf92527f9a55d6ebaea60c4
Max Gorin <nmcoder@gmail.com>
2013-03-01 06:11:11 +0800

    Custom NetzkeController POC

--- lib/netzke/core/railz/routes.rb
+++ lib/netzke/core/railz/routes.rb
@@ -3,8 +3,9 @@ module ActionDispatch::Routing
     # Enable routes for Netzke assets and endpoint calls. By default the URL is "/netzke", but this can be changed by providing an argument:
     #
     #     netzke "/some/path/netzke"
-    def netzke(prefix = "/netzke")
-      match "#{prefix}/:action(.:format)", to: "netzke", as: 'netzke'
+    def netzke(prefix = "/netzke", options = {})
+      controller = options[:controller] || :netzke
+      match "#{prefix}/:action(.:format)", to: controller.to_s, as: 'netzke'
     end
   end
 end
